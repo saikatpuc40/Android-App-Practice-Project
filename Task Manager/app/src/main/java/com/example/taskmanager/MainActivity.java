63af4db0f43bd083522eb5c12833cefc27dc17c9
Saikat Barua <saikatbarua2196@gmail.com>
2025-06-29 16:03:31 +0600

    add new file

---
+++ Task Manager/app/src/main/java/com/example/taskmanager/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.taskmanager;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.room.Room;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText inputFirstName, inputSecondName;
+    Button saveButton, addTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+
+
+    }
+}
